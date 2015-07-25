df9c67f6e1cb7b03155fd44e8dd0548a05fd3017
Thiago Leão Moreira <tmoreira2020@gmail.com>
2015-07-25 00:37:37 -0300

    Fix #22 - Add unit test for indexed properties

--- cores/portal-properties-prettier-core/src/test/java/br/com/thiagomoreira/liferay/plugins/portalpropertiesprettier/PortalPropertiesPrettierTest.java
+++ cores/portal-properties-prettier-core/src/test/java/br/com/thiagomoreira/liferay/plugins/portalpropertiesprettier/PortalPropertiesPrettierTest.java
@@ -115,6 +115,18 @@ public class PortalPropertiesPrettierTest {
 		Assert.assertEquals(expected, actual);
 	}
 
+	@Test
+	public void testIssue22() throws Exception {
+		PortalPropertiesPrettier prettier = new PortalPropertiesPrettier();
+		String expected = getContent("/portal-issue-22-expected.properties");
+		String actual = getContent("/portal-issue-22.properties");
+		Properties customProperties = PropertiesUtil.load(actual);
+
+		actual = prettier.prettify(customProperties, "6.2.3-ga4", false);
+
+		Assert.assertEquals(expected, actual);
+	}
+
 	protected String getContent(String path) throws IOException {
 		InputStream in = getClass().getResourceAsStream(path);
 
